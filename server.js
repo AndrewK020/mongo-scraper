01b907314e42a8be21c9e4f75431cccc811aed0b
Andrew Kelly <drew12396@gmail.com>
2019-07-12 16:48:09 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 3000;
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
+
+app.get("/", function(req, res) {
+   res.send("hello world");
+  });
+  
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
